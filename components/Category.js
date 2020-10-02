a42c53f41e242967812399be584cbe216d2d0827
n8ta <n8@u.northwestern.edu>
2020-10-01 19:02:00 -0500

    background color

--- components/Category.js
+++ components/Category.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import Course from './Course';
 
 
-const Category = ({ name, total, addedCourses, moveCourse, index }) => {
+const Category = ({name, total, addedCourses, moveCourse, index}) => {
 
-  const heading = total ? `${name} ${addedCourses.length} out of ${total}` : `${addedCourses.length} ${name}`
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{heading}</Text>
-      {addedCourses.map((course, i) => (
-        <Course key={i} index={i} moveCourse={moveCourse} categoryId={index} {...course} />
-      ))}
-    </View>
-  );
+    const heading = total ? `${name} ${addedCourses.length} out of ${total}` : `${addedCourses.length} ${name}`
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>{heading}</Text>
+            {addedCourses.map((course, i) => (
+                <Course key={i} index={i} moveCourse={moveCourse} categoryId={index} {...course} />
+            ))}
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
     container: {
-      padding: 15,
-      backgroundColor: '#fff',
-      marginBottom: 15,
-      marginTop: 15,
+        margin: 10,
+        marginBottom: 0,
+        padding: 15,
+        backgroundColor: '#dbdbdb',
     },
     text: {
-      color: '#000',
-      fontSize: 25
+        color: '#000',
+        fontSize: 25
     }
 });
 
