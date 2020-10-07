7535a64eab49caa7afbd1a24b9ebacc5bbd95135
n8ta <n8@u.northwestern.edu>
2020-10-06 19:50:17 -0500

    implement add category

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from './screens/HomeScreen'
+import AddCategoryScreen from "./screens/AddCategoryScreen";
 
 
 const Stack = createStackNavigator();
@@ -13,9 +14,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="AddCategoryScreen" component={AddCategoryScreen} />
       </Stack.Navigator>
-      
+
     </NavigationContainer>
   );
 }
