153c7121028799c073e92b9cde15514d64ff653a
Medet <medetkanmamyrov2022@u.northwestern.edu>
2020-10-06 20:25:23 -0500

    templates

---
+++ templates.js
@@ -0,0 +1,119 @@
+const weinbergCSTemplate = {categories: 
+  [
+    {
+      name: "Natural Sciences (Area I)",
+      total: 2,
+      addedCourses: []
+    },
+    {
+      name: "Formal Studies (Area II)",
+      total: 2,
+      addedCourses: []
+    },
+    {
+      name: "Social and Behavioral Sciences (Area III)",
+      total: 2,
+      addedCourses: []
+    },
+    {
+      name: "Historical Studies (Area IV)",
+      total: 2,
+      addedCourses: []
+    },
+    {
+      name: "Ethics and Values (Area V)",
+      total: 2,
+      addedCourses: []
+    },
+    {
+      name: "Literature and Fine Arts (Area VI)",
+      total: 2,
+      addedCourses: []
+    },
+    {
+      name: "Core Courses",
+      total: 6,
+      addedCourses: []
+    },
+    {
+      name: "Breadth Courses",
+      total: 5,
+      addedCourses: []
+    },
+    {
+      name: "Technical Electives",
+      total: 6,
+      addedCourses: []
+    },
+    {
+      name: "Project Courses",
+      total: 2,
+      addedCourses: []
+    }
+  ],
+  name: "Weinberg CS Template"};
+
+const mcTemplate = {categories: 
+  [
+    {
+      name: "Math",
+      total: 4,
+      addedCourses: []
+    },
+    {
+      name: "Engineering Analysis and Computer Proficiency courses",
+      total: 4,
+      addedCourses: []
+    },
+    {
+      name: "Basic Science",
+      total: 4,
+      addedCourses: []
+    },
+    {
+      name: "DTC",
+      total: 3,
+      addedCourses: []
+    },
+    {
+      name: "Basic Engineering",
+      total: 5,
+      addedCourses: []
+    },
+    {
+      name: "Social Sciences/Humanities",
+      total: 7,
+      addedCourses: []
+    },
+    {
+      name: "Unrestricted Electives",
+      total: 5,
+      addedCourses: []
+    },
+    {
+      name: "Core Courses",
+      total: 6,
+      addedCourses: []
+    },
+    {
+      name: "Breadth Courses",
+      total: 5,
+      addedCourses: []
+    },
+    {
+      name: "Technical Electives",
+      total: 6,
+      addedCourses: []
+    },
+    {
+      name: "Project Courses",
+      total: 2,
+      addedCourses: []
+    }
+  ],
+  name: "McCormick CS Template"
+}
+
+const templates = [weinbergCSTemplate, mcTemplate];
+
+export default templates;
