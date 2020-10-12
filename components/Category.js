549b80cc5dd7ab1b0e3bc4cc785404963b074882
Neha Sharma <nehasharma2021@u.northwestern.edu>
2020-10-12 17:45:09 -0500

    Adding styling to category headers

--- components/Category.js
+++ components/Category.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
 import { Card, List } from 'react-native-paper';
+import { StyleSheet, Text, View } from 'react-native';
 
 import Course from './Course';
+import React from 'react';
 
 const Category = ({ name, total, addedCourses, moveCourse, index }) => {
-  const heading = total ? `${name} ${addedCourses.length} out of ${total}` : `${addedCourses.length} ${name}`;
+  const styledHeading = (
+    <View style={styles.header}>
+      {total ? (
+        <>
+          <Text>{name}</Text>
+          <Text style={styles.numCourses}>
+            {`${addedCourses.length} out of ${total}`}{' '}
+          </Text>
+        </>
+      ) : (
+        <>
+          <Text style={styles.numCourses}>{`${addedCourses.length} `}</Text>
+          <Text>{name}</Text>
+        </>
+      )}
+    </View>
+  );
   const [expanded, setExpanded] = React.useState(true);
   const handlePress = () => setExpanded(!expanded);
 
   return (
     <Card style={styles.category}>
-      <List.Accordion title={heading} style={styles.list} titleStyle={styles.listTitle} expanded={expanded} onPress={handlePress}>
+      <List.Accordion
+        title={styledHeading}
+        style={styles.list}
+        titleStyle={styles.listTitle}
+        expanded={expanded}
+        onPress={handlePress}
+      >
         {addedCourses.map((course, i) => (
-          <Course key={i} index={i} moveCourse={moveCourse} categoryId={index} {...course} />
+          <Course
+            key={i}
+            index={i}
+            moveCourse={moveCourse}
+            categoryId={index}
+            {...course}
+          />
         ))}
       </List.Accordion>
     </Card>
@@ -21,16 +49,24 @@ const Category = ({ name, total, addedCourses, moveCourse, index }) => {
 };
 
 const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+  },
+  numCourses: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: 'darkgrey',
+  },
   category: {
     marginBottom: 13,
   },
   list: {
-    backgroundColor: "#e3e1e1",
+    backgroundColor: '#e3e1e1',
     padding: 10,
   },
   listTitle: {
-    fontSize: 18
-  }
+    fontSize: 18,
+  },
 });
 
 export default Category;
--- components/Course.js
+++ components/Course.js
@@ -1,11 +1,18 @@
+import { Divider, List, Menu } from 'react-native-paper';
 import React, { useContext, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Menu, Divider, List } from 'react-native-paper'
+import { StyleSheet, View } from 'react-native';
 
 import CategoryContext from '../CategoryContext';
 
-const Course = ({ id, title, index, moveCourse, categoryId }) => {
-  const categories = useContext(CategoryContext)
+const Course = ({
+  number,
+  department,
+  title,
+  index,
+  moveCourse,
+  categoryId,
+}) => {
+  const categories = useContext(CategoryContext);
   const [visible, setVisible] = useState(false);
   const menuToggle = () => setVisible(!visible);
   const courseTrigger = (
@@ -14,26 +21,32 @@ const Course = ({ id, title, index, moveCourse, categoryId }) => {
         style={styles.list}
         titleStyle={styles.listTitle}
         descriptionStyle={styles.listDescription}
-        title={id} description={title}
+        title={number}
+        description={title}
         onPress={menuToggle}
-        left={props => <List.Icon {...props} color="#3498db" icon="menu" />}
+        left={(props) => <List.Icon {...props} color='#3498db' icon='menu' />}
       />
-      <Divider inset={false} style={{padding: .5}}/>
+      <Divider inset={false} style={{ padding: 0.5 }} />
     </View>
-  )
+  );
 
   return (
     <Menu
       contentStyle={styles.menuItems}
       visible={visible}
       onDismiss={menuToggle}
-      anchor={courseTrigger}>
-      {categories.map((category, i) =>
-        <Menu.Item key={i} title={category.name}
+      anchor={courseTrigger}
+    >
+      {categories.map((category, i) => (
+        <Menu.Item
+          key={i}
+          title={category.name}
           onPress={() => {
-            moveCourse(categoryId, index, i)
-            menuToggle()
-          }} />)}
+            moveCourse(categoryId, index, i);
+            menuToggle();
+          }}
+        />
+      ))}
     </Menu>
   );
 };
@@ -44,13 +57,12 @@ const styles = StyleSheet.create({
   },
   list: {
     paddingHorizontal: 12,
-    paddingVertical: 4
+    paddingVertical: 4,
   },
   listTitle: {
     fontSize: 17,
   },
-  listDescription: {
-  }
+  listDescription: {},
 });
 
 export default Course;
