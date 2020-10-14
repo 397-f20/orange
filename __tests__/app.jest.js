1f9c95a5fae6d0b12fa09e744d54d736572eae39
n8ta <n8@u.northwestern.edu>
2020-10-14 16:00:08 -0500

    turn off console.warn and error

--- __tests__/app.jest.js
+++ __tests__/app.jest.js
@@ -5,6 +5,8 @@ import App from '../App';
 jest.useFakeTimers();
 
 describe('<App />', () => {
+  console.error = jest.fn();
+  console.warn = jest.fn();
   it('has 1 child', () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree.children.length).toBe(1);
