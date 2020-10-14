698a61c1858392d41f88dd974fc41014ee144180
n8ta <n8@u.northwestern.edu>
2020-10-14 16:13:32 -0500

    use async for callback

--- __tests__/app.jest.js
+++ __tests__/app.jest.js
@@ -2,13 +2,12 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../App';
 
-jest.useFakeTimers();
+jest.useFakeTimers()
 
 describe('<App />', () => {
-  console.error = jest.fn();
-  console.warn = jest.fn();
-  it('has 1 child', () => {
+  jest.useFakeTimers();
+  it('has 1 child', async () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree.children.length).toBe(1);
-  }, async () => {});
+  });
 });
