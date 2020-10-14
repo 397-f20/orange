3fcf4cb337e17bbb14315c050bb8b4d03e03765a
Neha Sharma <nehasharma2021@u.northwestern.edu>
2020-10-14 15:12:47 -0500

    Mob: Added Jest Config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+  transformIgnorePatterns: ['node_modules/*'],
+};
