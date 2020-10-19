854f48669a4fe3d9f72326130afb9d62ad4b6dc9
n8ta <n8@u.northwestern.edu>
2020-10-18 23:41:10 -0500

    act

--- __tests__/course.test.js
+++ __tests__/course.test.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import renderer, {act} from 'react-test-renderer';
 import Course from '../components/Course';
 import CategoryContext from "../CategoryContext";
 import templates from "../templates";
+import App from "../App";
 
 const props = {
     number: '340',
     title: 'Networking',
     index: 5,
-    moveCourse: () => {},
+    moveCourse: () => {
+    },
     categoryId: 6,
 }
 
 describe('<Course/>', () => {
     it('has includes text: CS 340', async () => {
         jest.useFakeTimers();
-        const tree = renderer.create(
-            <CategoryContext.Provider value={templates[0].categories}>
-                <Course {...props} />
-            </CategoryContext.Provider>).toJSON();
+        let tree;
+
+        await act(async () => {
+            tree = renderer.create(
+                <CategoryContext.Provider value={templates[0].categories}>
+                    <Course {...props} />
+                </CategoryContext.Provider>)
+        });
+        tree = tree.toJSON()
+
         expect(JSON.stringify(tree).indexOf("CS 340")).toBeGreaterThan(-1);
     });
 });
