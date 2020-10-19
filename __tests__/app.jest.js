f085ad2d58829974f45ca99df674ff4b9051e842
n8ta <n8@u.northwestern.edu>
2020-10-18 21:30:53 -0500

    fix act warning

--- __tests__/app.jest.js
+++ __tests__/app.jest.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import renderer, {act} from 'react-test-renderer';
 import App from '../App';
 
-jest.useFakeTimers()
 
 describe('<App />', () => {
-  jest.useFakeTimers();
-  it('has 1 child', async () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
-  });
+    jest.useFakeTimers();
+    it('has 1 child', async () => {
+        let tree;
+        await act(async () => {
+            tree = renderer.create(<App/>)
+        });
+        tree = tree.toJSON()
+        expect(tree.children.length).toBe(1);
+    })
 });
