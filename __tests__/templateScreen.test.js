a07e783c8670aaee1726baab94fe0600e93e1eca
Philip Clement <pclement803@gmail.com>
2020-10-20 18:48:19 -0500

    testing templateScreen

---
+++ __tests__/templateScreen.test.js
@@ -0,0 +1,19 @@
+import renderer, { act } from 'react-test-renderer';
+
+import TemplateScreen from '../screens/TemplateScreen'
+import React from 'react';
+
+
+describe('<TemplateScreen />', () => {
+  it('renders TemplateScreen', async () => {
+    jest.useFakeTimers();
+    let tree;
+
+    await act(async () => {
+      tree = renderer.create(<TemplateScreen />);
+    });
+    tree = tree.toJSON();
+
+    expect(tree.type).toBe('RCTSafeAreaView');
+  });
+});
