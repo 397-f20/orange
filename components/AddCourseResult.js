0f7341712b8796761f26d1145ca49ea23af86cf1
n8ta <n8@u.northwestern.edu>
2020-11-04 15:06:00 -0600

    not working

---
+++ components/AddCourseResult.js
@@ -0,0 +1,42 @@
+import {List, Menu} from "react-native-paper";
+import React, { useState } from 'react'
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+
+const AddCourseResult = ({idx, categories, course, addCourse}) => {
+    const [visible, setVisible] = useState(false)
+    return (<Menu contentStyle={styles.menuItems} visible={visible}
+          anchor={
+              <List.Item
+                  onPress={() => setVisible(true)}
+                  key={idx}
+                  title={`${course.subject} ${course.number} - ${course.title}`}
+                  left={(props) => (
+                      <List.Icon
+                          {...props}
+                          icon={'plus'}
+                          style={styles.listIconStyle}/>)}/>}>
+        {categories.map((category, catIdx) => (<Menu.Item
+            key={catIdx}
+            title={category.name}
+            onPress={() => {addCourse(course, catIdx);}}
+        />))}
+    </Menu>)
+}
+
+const styles = StyleSheet.create({
+    dialog: {
+        height: 600,
+    },
+    addCourseContainer: {
+        margin: 20,
+        marginTop: 30,
+        height: '100%',
+    },
+    listIconStyle: {
+        marginLeft: 0,
+        marginRight: 0,
+        width: 20,
+    },
+});
+
+export default AddCourseResult
--- screens/AddCourseScreen.js
+++ screens/AddCourseScreen.js
@@ -1,127 +1,85 @@
-import { Button, Dialog, List, Searchbar } from 'react-native-paper';
-import React, { useContext, useEffect, useState } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
-
+import {Button, Dialog, List, Menu, Searchbar} from 'react-native-paper';
+import React, {useContext, useEffect, useState} from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import AddCourseResult from '../components/AddCourseResult'
 import CategoryContext from '../CategoryContext';
-import { mockCourses } from '../mockCourses';
+import {mockCourses} from '../mockCourses';
 
-const AddCourseScreen = ({ navigation }) => {
-  const { categories } = useContext(CategoryContext);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [matches, setMatches] = useState([]);
-  const [visible, setVisible] = useState(false);
-  const [selectedCourse, setSelectedCourse] = useState({});
-  const [selectedCategory, setSelectedCategory] = useState(-1);
+const AddCourseScreen = ({navigation}) => {
+    const {categories} = useContext(CategoryContext);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [matches, setMatches] = useState([]);
 
-  const updateQuery = (str) => {
-    const searchCurrentMatches =
-      matches.length > 0 && str.length > searchQuery.length;
-    setSearchQuery(str);
-    if (str.length < 3) {
-      setMatches([]);
-      return;
-    }
-    const searchLower = str.toLowerCase();
-    setMatches(
-      (searchCurrentMatches ? matches : mockCourses).filter((course) => {
-        return (
-          course.number.indexOf(str) !== -1 ||
-          course.subject.toLowerCase().indexOf(searchLower) !== -1 ||
-          course.title.toLowerCase().indexOf(searchLower) !== -1
+    const updateQuery = (str) => {
+        const searchCurrentMatches =
+            matches.length > 0 && str.length > searchQuery.length;
+        setSearchQuery(str);
+        if (str.length < 3) {
+            setMatches([]);
+            return;
+        }
+        const searchLower = str.toLowerCase();
+        setMatches(
+            (searchCurrentMatches ? matches : mockCourses).filter((course) => {
+                return (
+                    course.number.indexOf(str) !== -1 ||
+                    course.subject.toLowerCase().indexOf(searchLower) !== -1 ||
+                    course.title.toLowerCase().indexOf(searchLower) !== -1
+                );
+            })
         );
-      })
+    };
+
+    const addCourse = (selectedCourse, selectedCategory) => {
+        setSearchQuery('');
+        navigation.navigate('HomeScreen', {
+            addSelectedCourse: selectedCourse,
+            addCourseCategory: selectedCategory,
+        });
+    };
+
+    return (
+        <>
+            <ScrollView style={styles.addCourseContainer}>
+                <SafeAreaView>
+                    <Searchbar
+                        placeholder='Search Course'
+                        onChangeText={updateQuery}
+                        value={searchQuery}
+                    />
+                </SafeAreaView>
+            </ScrollView>
+            <View accessibilityLabel={'addcoursedialog'}>
+
+                {matches.slice(0, 10).map((course, i) => (
+                    <AddCourseResult idx={i} course={course} categories={categories} addCourse={addCourse()} />
+                ))}
+            </View>
+        </>
     );
-  };
-
-  const addCourse = () => {
-    setMatches([]);
-    setSearchQuery('');
-    navigation.navigate('HomeScreen', {
-      addSelectedCourse: selectedCourse,
-      addCourseCategory: selectedCategory,
-    });
-  };
-
-  const cancelDialog = () => {
-    setSelectedCourse({});
-    setVisible(false);
-  };
-
-  return (
-    <>
-      <ScrollView style={styles.addCourseContainer}>
-        <SafeAreaView>
-          <Searchbar
-            placeholder='Search Course'
-            onChangeText={updateQuery}
-            value={searchQuery}
-          />
-          {matches.slice(0, 10).map((course, i) => (
-            <List.Item
-              key={i}
-              title={`${course.subject} ${course.number} - ${course.title}`}
-              left={(props) => (
-                <List.Icon
-                  {...props}
-                  icon={'plus'}
-                  style={styles.listIconStyle}
-                />
-              )}
-              onPress={() => {
-                setVisible(true);
-                setSelectedCourse(course);
-              }}
-            />
-          ))}
-        </SafeAreaView>
-      </ScrollView>
-      <View accessibilityLabel={'addcoursedialog'}>
-      <Dialog
-          visible={visible} onDismiss={() => setVisible(false)}>
-        <Dialog.Title>Add Course to Category</Dialog.Title>
-        <Dialog.ScrollArea style={styles.dialog}>
-          {categories.map((category, i) => (
-            <List.Item
-              key={i}
-              title={category.name}
-              onPress={() => setSelectedCategory(i)}
-              style={selectedCategory === i ? styles.selected : styles.notSelected }
-            />
-          ))}
-        </Dialog.ScrollArea>
-        <Dialog.Actions style={styles.dialogActions}>
-          <Button onPress={cancelDialog}>Cancel</Button>
-          <Button accessibilityLabel={'addcoursefrommodal'} onPress={addCourse}>Add Course</Button>
-        </Dialog.Actions>
-      </Dialog>
-      </View>
-    </>
-  );
 };
 
 const styles = StyleSheet.create({
-  dialogActions: {
-    marginBottom: 30
-  },
-  dialog: {
-    height: 600,
-  },
-  addCourseContainer: {
-    margin: 20,
-    marginTop: 30,
-    height: '100%',
-  },
-  listIconStyle: {
-    marginLeft: 0,
-    marginRight: 0,
-    width: 20,
-  },
-  selected: {
-    backgroundColor: '#bbbbbb',
-  },
-  notSelected: {
-
-  }
+    dialogActions: {
+        marginBottom: 30
+    },
+    dialog: {
+        height: 600,
+    },
+    addCourseContainer: {
+        margin: 20,
+        marginTop: 30,
+        height: '100%',
+    },
+    listIconStyle: {
+        marginLeft: 0,
+        marginRight: 0,
+        width: 20,
+    },
+    selected: {
+        backgroundColor: '#bbbbbb',
+    },
+    notSelected: {}
 });
 
 export default AddCourseScreen;
