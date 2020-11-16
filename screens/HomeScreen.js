1c1e17c157a047d5b739f2b0c7d2bb55d286df86
Philip Clement <pclement803@gmail.com>
2020-11-16 17:08:19 -0600

    changing add category button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,11 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { SafeAreaView, ScrollView, StyleSheet, View, Text } from "react-native";
 
-import AsyncStorage from '@react-native-community/async-storage';
-import { Button } from 'react-native-paper';
-import Category from '../components/Category';
-import CategoryContext from '../CategoryContext';
-import { mockCourses } from '../mockCourses';
+import AsyncStorage from "@react-native-community/async-storage";
+import { Card, Avatar } from "react-native-paper";
+import Category from "../components/Category";
+import CategoryContext from "../CategoryContext";
+import { mockCourses } from "../mockCourses";
 
 const storeCategories = (name, categories) => {
   try {
@@ -16,7 +16,7 @@ const storeCategories = (name, categories) => {
 };
 
 const unallocated = {
-  name: 'Unallocated',
+  name: "Unallocated",
   total: null,
   addedCourses: [],
 };
@@ -39,7 +39,9 @@ const HomeScreen = ({ navigation, route }) => {
       // fetching categories from react storage
       let storageCategories;
       try {
-        storageCategories = JSON.parse(await AsyncStorage.getItem(template.name));
+        storageCategories = JSON.parse(
+          await AsyncStorage.getItem(template.name)
+        );
       } catch (e) {
         storageCategories = null;
         console.error(e);
@@ -76,9 +78,9 @@ const HomeScreen = ({ navigation, route }) => {
 
   const removeCourse = (categoryIdx, courseIdx) => {
     let newCategories = categories.slice(0);
-    newCategories[categoryIdx].addedCourses = newCategories[categoryIdx].addedCourses.filter(
-      (course, i) => i != courseIdx
-    );
+    newCategories[categoryIdx].addedCourses = newCategories[
+      categoryIdx
+    ].addedCourses.filter((course, i) => i != courseIdx);
     storeCategories(storageKey, newCategories);
     setCurrentCategories(newCategories);
   };
@@ -89,30 +91,47 @@ const HomeScreen = ({ navigation, route }) => {
       newCategories.push(newCategory);
       storeCategories(storageKey, newCategories);
       setCurrentCategories(newCategories);
-      navigation.navigate('HomeScreen');
+      navigation.navigate("HomeScreen");
     },
     [categories, storageKey, storeCategories, setCurrentCategories]
   );
 
+  //   <Button
+  //   mode='contained'
+  //   labelStyle={styles.buttonStyle}
+  //   contentStyle={styles.buttonWrapStyle}
+  //   onPress={() => navigation.navigate('AddCategoryScreen', { addCategory })}
+  // >
+  //   {`   Add Category    `}
+  // </Button>
+
+  const AddCategoryButton = () => (
+    <Card
+    onPress={() => navigation.navigate('AddCategoryScreen', { addCategory })}
+    style={styles.addCategory}
+    >
+    <Avatar.Icon size={50} icon="plus"/>
+    </Card>
+  );
+
   return (
     <>
-      <Button
-        mode='contained'
-        labelStyle={styles.buttonStyle}
-        contentStyle={styles.buttonWrapStyle}
-        onPress={() => navigation.navigate('AddCategoryScreen', { addCategory })}
-      >
-        {`   Add Category    `}
-      </Button>
       <ScrollView>
         <View style={styles.container}>
           <ScrollView>
             <View style={styles.categoryContainer}>
               {categories.map((category, i) => (
                 <View style={styles.category}>
-                  <Category removeCourse={removeCourse} moveCourse={moveCourse} key={i} index={i} {...category} />
+                  <Category
+                    removeCourse={removeCourse}
+                    moveCourse={moveCourse}
+                    key={i}
+                    index={i}
+                    {...category}
+                  />
                 </View>
               ))}
+              <AddCategoryButton />
             </View>
           </ScrollView>
         </View>
@@ -123,24 +142,36 @@ const HomeScreen = ({ navigation, route }) => {
 
 const styles = StyleSheet.create({
   buttonStyle: {
-    color: 'white',
+    color: "white",
   },
   buttonWrapStyle: {
     paddingBottom: 5,
     paddingTop: 5,
   },
   categoryContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+    flexDirection: "row",
+    flexWrap: "wrap",
     flex: 1,
-    justifyContent: 'center',
-    width: '100%',
+    justifyContent: "center",
+    width: "100%",
   },
   category: {
     width: 175,
     paddingTop: 10,
     margin: 0,
     padding: 0,
+  },
+  addCategory: {
+    width: 175,
+    borderRadius: 5,
+    marginBottom: 3,
+    marginHorizontal: 3,
+    paddingTop: 7,
+    paddingBottom: 7,
+    marginTop: 10,
+    shadowOffset: { height: 3, width: 0 },
+    shadowRadius: 6,
+    shadowOpacity: 0.24,
   },
 });
 
