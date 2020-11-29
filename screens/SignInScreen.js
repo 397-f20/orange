0925ee8d8019483718819238d9b8bd1f80dd3482
Neha Sharma <nehasharma2021@u.northwestern.edu>
2020-11-29 16:53:04 -0600

    Added sign-screen changes

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,94 +1,127 @@
-import {SafeAreaView, ScrollView} from 'react-native'
-import Form from '../components/expo-form-starter'
 import * as Yup from 'yup';
-import { firebase } from "../firebase";
-import React, {useState, useContext} from 'react';
-import {Button} from 'react-native-paper'
+
+import { Image, SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+
+import { Button } from 'react-native-paper';
+import Form from '../components/expo-form-starter';
+import { firebase } from '../firebase';
 
 const validationSchema = Yup.object().shape({
-    email: Yup.string()
-        .required('Please enter a valid email')
-        .email()
-        .label('Email'),
-    password: Yup.string()
-        .required()
-        .min(6, 'Password must have at least 6 characters')
-        .label('Password'),
-    confirm: Yup.string()
-        .oneOf([Yup.ref('password'), null], 'Confirmation password must match password'),
+  email: Yup.string().required('Please enter a valid email').email().label('Email'),
+  password: Yup.string().required().min(6, 'Password must have at least 6 characters').label('Password'),
+  confirm: Yup.string().oneOf([Yup.ref('password'), null], 'Confirmation password must match password'),
 });
 
+const LOGIN = 'LOGIN';
+const SIGN_UP = 'SIGN_UP';
 
-const LOGIN = 'LOGIN'
-const SIGN_UP = 'SIGN_UP'
+const SignInScreen = ({ navigation }) => {
+  const [signInError, setSignInError] = useState('');
+  const [mode, setMode] = useState(LOGIN);
 
-const SignInScreen = ({navigation}) => {
-    const [signInError, setSignInError] = useState('')
-    const [mode, setMode] = useState(LOGIN)
-
-    const changeMode = () => {
-        if (mode === LOGIN) {
-            setMode(SIGN_UP)
-        } else {
-            setMode(LOGIN)
-        }
+  const changeMode = () => {
+    if (mode === LOGIN) {
+      setMode(SIGN_UP);
+    } else {
+      setMode(LOGIN);
     }
+  };
 
-    const handleOnSubmit = async (values) => {
-        if (mode === SIGN_UP) {
-            await firebase.auth().createUserWithEmailAndPassword(values.email, values.password);
-        } else {
-            await firebase.auth().signInWithEmailAndPassword(values.email, values.password);
-        }
-        navigation.navigate('TemplateScreen', {})
+  const handleOnSubmit = async (values) => {
+    if (mode === SIGN_UP) {
+      await firebase.auth().createUserWithEmailAndPassword(values.email, values.password);
+    } else {
+      await firebase.auth().signInWithEmailAndPassword(values.email, values.password);
     }
+    navigation.navigate('TemplateScreen', {});
+  };
 
-    return (
-        <SafeAreaView>
-            <ScrollView>
-                <Form
-                    initialValues={{
-                        email: '',
-                        password: '',
-                        confirmPassword: '',
-                    }}
-                    validationSchema={validationSchema}
-                    onSubmit={handleOnSubmit}
-                >
-                    <Form.Field
-                        name="email"
-                        leftIcon="email"
-                        placeholder="Enter email"
-                        autoCapitalize="none"
-                        keyboardType="email-address"
-                        textContentType="emailAddress"
-                    />
-                    <Form.Field
-                        name="password"
-                        leftIcon="lock"
-                        placeholder="Enter password"
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        secureTextEntry={true}
-                        textContentType="password"
-                    />
-                    { mode === SIGN_UP  && (
-                    <Form.Field
-                        name="confirm"
-                        leftIcon="lock"
-                        placeholder="Confirm password"
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        secureTextEntry={true}
-                        textContentType="password"
-                    />)}
-                    <Form.Button title={mode === LOGIN ? 'Log In' : 'Sign Up' }/>
-                    {<Form.ErrorMessage error={signInError} visible={true} />}
-                </Form>
-                <Button mode='text' onPress={changeMode}>{`${mode === LOGIN ? "Don't have an account? Sign up here" : "Return to login"}`}</Button>
-            </ScrollView>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.logoContainer}>
+          <Image source={require('../resources/ReqTrack.png')} />
+        </View>
+        <View style={styles.form}>
+          <Form
+            initialValues={{
+              email: '',
+              password: '',
+              confirmPassword: '',
+            }}
+            validationSchema={validationSchema}
+            onSubmit={handleOnSubmit}
+          >
+            <Form.Field
+              name='email'
+              leftIcon='email'
+              placeholder='Enter email'
+              autoCapitalize='none'
+              keyboardType='email-address'
+              textContentType='emailAddress'
+            />
+            <Form.Field
+              name='password'
+              leftIcon='lock'
+              placeholder='Enter password'
+              autoCapitalize='none'
+              autoCorrect={false}
+              secureTextEntry={true}
+              textContentType='password'
+            />
+            {mode === SIGN_UP && (
+              <Form.Field
+                name='confirm'
+                leftIcon='lock'
+                placeholder='Confirm password'
+                autoCapitalize='none'
+                autoCorrect={false}
+                secureTextEntry={true}
+                textContentType='password'
+              />
+            )}
+            <Form.Button style={styles.primaryButton} title={mode === LOGIN ? 'Log In' : 'Sign Up'} />
+            {<Form.ErrorMessage error={signInError} visible={true} />}
+          </Form>
+          <Button mode='text' onPress={changeMode} style={styles.secondaryButton}>
+            {`${mode === LOGIN ? "Don't have an account? Sign up here" : 'Return to login'}`}
+          </Button>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    margin: 20,
+  },
+  scrollContainer: {
+    flex: 1,
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  form: {
+    flex: 4,
+    justifyContent: 'flex-start',
+  },
+  primaryButton: {
+    marginVertical: 20,
+    width: '100%',
+  },
+  secondaryButton: {
+    flex: 1,
+    flexGrow: 1,
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  logoContainer: {
+    flex: 1,
+  },
+});
 
 export default SignInScreen;
