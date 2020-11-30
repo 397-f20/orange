acee7424901ba51b4251851599330bb0213e7d12
Neha Sharma <nehasharma2021@u.northwestern.edu>
2020-11-29 20:56:59 -0600

    Changed Add Course styling and updated course jest test to run successfully

--- __tests__/course.test.js
+++ __tests__/course.test.js
@@ -1,18 +1,19 @@
-import React from 'react';
 import renderer, { act } from 'react-test-renderer';
+
 import Course from '../components/Course';
-import CategoryContext from '../CategoryContext';
+import { DraxProvider } from 'react-native-drax';
+import PlanContext from '../PlanContext';
+import React from 'react';
 import templates from '../templates';
 
 const props = {
-    subject:'CS',
-    number: '340',
-    title: 'Networking',
-    index: 5,
-    moveCourse: () => {
-    },
-    categoryId: 6,
-}
+  subject: 'CS',
+  number: '340',
+  title: 'Networking',
+  index: 5,
+  moveCourse: () => {},
+  categoryId: 6,
+};
 
 describe('<Course/>', () => {
   it('has includes text: CS 340', async () => {
@@ -21,11 +22,11 @@ describe('<Course/>', () => {
 
     await act(async () => {
       tree = renderer.create(
-        <CategoryContext.Provider
-          value={{ categories: templates[0].categories }}
-        >
-          <Course {...props} />
-        </CategoryContext.Provider>
+        <DraxProvider>
+          <PlanContext.Provider value={{ currentPlan: templates[0].categories }}>
+            <Course {...props} />
+          </PlanContext.Provider>
+        </DraxProvider>
       );
     });
     tree = tree.toJSON();
